1688f69f408187341f08499f4a1b4bab368c3c31
Midway65 <comweb+git@pm.me>
2026-05-07 12:27:48 -0700

    chore: regen connectorContent.ts post v5.8.12 merge build

--- src/utils/connectorContent.ts
+++ src/utils/connectorContent.ts
@@ -5,7 +5,7 @@
  * DO NOT EDIT MANUALLY - This file is regenerated during the build process.
  * To update, modify connector.ts and rebuild.
  *
- * Generated: 2026-05-07T18:52:00.448Z
+ * Generated: 2026-05-07T19:27:40.348Z
  */
 
 export const CONNECTOR_JS_CONTENT = `"use strict";
